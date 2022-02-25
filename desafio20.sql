61aaf86b371c4f0d056783c92104e278777ea22b
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-02-25 08:36:28 -0300

    passa no requisito 20

---
+++ desafio20.sql
@@ -0,0 +1,4 @@
+INSERT INTO order_details
+(order_id, product_id, quantity, unit_price, purchase_order_id, inventory_id, discount, status_id, date_allocated)
+VALUES
+(69, 80, 15.0000, 15.0000, NULL, 129, 0, 2, NULL);
